be7d810b280d16c2bafc05901ddde9c3fad47946
Alik Nigmatullin <NigAlik020503@yandex.ru>
2023-11-25 17:32:13 +0300

    first page

---
+++ my-app/src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './MainPage.module.scss'
+
+const MainPage: React.FC = () => {
+  return (
+    <div className={styles.example}>MainPage</div>
+  )
+}
+
+export default MainPage
